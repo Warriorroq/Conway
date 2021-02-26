2eb5e0db515076bc8858e3c5928d72e87d596001
Warriorroq <Warriorroq@gmail.com>
2021-02-26 19:15:52 +0200

    Conway

---
+++ Conway/Conway/Conway.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int R, L, next, current;
+    vector<int> currentLine, lastLine;
+    cin >> R >> L;
+
+    currentLine.push_back(R);
+    for (int i = 1; i < L;i++) {
+        int j = 0;
+        while (j < currentLine.size()) {
+            current = 1;
+            next = currentLine[j++];
+            while (j < currentLine.size() && currentLine[j] == currentLine[j - 1]) {
+                current++;
+                j++;
+            }
+            lastLine.push_back(current);
+            lastLine.push_back(next);
+        }
+        currentLine = lastLine;
+        lastLine.clear();
+    }
+
+    for (int i = 0; i < currentLine.size() - 1; i++)
+        cout << currentLine[i] << " ";
+
+    cout << currentLine[currentLine.size() - 1] << endl;
+}
